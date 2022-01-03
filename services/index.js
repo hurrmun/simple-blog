974bc347c3736be45740130d0cd015919dc2be35
hermanlyx <61788143+hurrmun@users.noreply.github.com>
2022-01-03 12:58:48 +0800

    added get post details in services and slug file

--- services/index.js
+++ services/index.js
@@ -38,6 +38,41 @@ export const getPosts = async () => {
   return result.postsConnection.edges;
 };
 
+export const getPostDetails = async (slug) => {
+  const query = gql`
+    query GetPostDetails($slug: String!) {
+      post(where: { slug: $slug }) {
+        author {
+          bio
+          name
+          id
+          photo {
+            url
+          }
+        }
+        createdAt
+        slug
+        title
+        excerpt
+        featuredImage {
+          url
+        }
+        categories {
+          name
+          slug
+        }
+        content {
+          raw
+        }
+      }
+    }
+  `;
+
+  const result = await request(graphqlAPI, query, { slug });
+
+  return result.post;
+};
+
 export const getRecentPosts = async () => {
   const query = gql`
         query GetPostDetails() {
@@ -60,24 +95,27 @@ export const getRecentPosts = async () => {
   return result.posts;
 };
 
-export const getSimilarPosts = async () => {
+export const getSimilarPosts = async (categories, slug) => {
   const query = gql`
-        query GetPostDetails($slug: String!, $categories:[String!]) {
-            posts(
-                where: {slug_not: $slug, AND: {categories.some: { slug_in: $categories}}}
-                last: 3
-                ) {
-                title
-                featuredImage {
-                    url
-                }
-                createdAt
-                slug
-                }
+    query GetPostDetails($slug: String!, $categories: [String!]) {
+      posts(
+        where: {
+          slug_not: $slug
+          AND: { categories_some: { slug_in: $categories } }
         }
-    `;
+        last: 3
+      ) {
+        title
+        featuredImage {
+          url
+        }
+        createdAt
+        slug
+      }
+    }
+  `;
 
-  const result = await request(graphqlAPI, query);
+  const result = await request(graphqlAPI, query, { categories, slug });
 
   return result.posts;
 };
